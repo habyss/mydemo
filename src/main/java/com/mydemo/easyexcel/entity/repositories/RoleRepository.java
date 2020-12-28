61e2dcd5de667bd539127b12f8250b2b005f3bd6
kun.han <kun.han@mytian.com.cn>
2020-12-28 14:23:59 +0800

    删除未知

--- src/main/java/com/mydemo/easyexcel/entity/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mydemo.easyexcel.entity.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface RoleRepository extends JpaRepository<Role, Long>, QuerydslPredicateExecutor<Role> {
-}
--- src/main/java/repositories/RoleRepository.java
+++
@@ -1,5 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-public interface RoleRepository extends JpaRepository<Role, Long>, QuerydslPredicateExecutor<Role> {
-}
