52cc1070e16f1d0e15e13041c66306cad6b890e6
kun.han <kun.han@mytian.com.cn>
2020-12-28 16:09:32 +0800

    localDateTime

--- easyexcel/src/main/java/com/mydemo/easyexcel/config/CustomDateConfig.java
+++ easyexcel/src/main/java/com/mydemo/easyexcel/config/CustomDateConfig.java
@@ -1,62 +1,109 @@
 package com.mydemo.easyexcel.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.fasterxml.jackson.databind.util.Converter;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.IOException;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.Objects;
 
 /**
  * @author kun.han
  */
 @Configuration
 public class CustomDateConfig implements WebMvcConfigurer {
-    
+
     /**
-     * Json序列化和反序列化转换器，用于转换Post请求体中的json以及将我们的对象序列化为返回响应的json
+     * 1> Json序列化和反序列化转换器，用于转换Post请求体中的json以及将我们的对象序列化为返回响应的json
      */
     @Bean
     public ObjectMapper objectMapper() {
+        // 序列化的通用配置 可默认不做修改
         ObjectMapper objectMapper = new ObjectMapper();
-        //不显示为null的字段
+        // 不显示为null的字段
         objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         // 过滤对象的null属性.
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        //忽略transient
+        // 忽略transient
         objectMapper.configure(MapperFeature.PROPAGATE_TRANSIENT_MARKER, true);
-
         objectMapper.disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE);
 
         //LocalDateTime系列序列化和反序列化模块，继承自jsr310，我们在这里修改了日期格式
         JavaTimeModule javaTimeModule = new JavaTimeModule();
-        // LocalDateTime 这里只需要LocalDateTime 如果需要转其他的,相应放开注释, 并在上面两个类中适当修改
-        javaTimeModule.addSerializer(LocalDateTime.class, new CustomDateSerializer.LocalDateTimeSerializer());
-        javaTimeModule.addDeserializer(LocalDateTime.class,new CustomDateDeserializer.LocalDateTimeDeserializer());
-        // // LocalDate
-        // javaTimeModule.addSerializer(LocalDate.class, new CustomDateSerializer.LocalDateSerializer());
-        // javaTimeModule.addDeserializer(LocalDate.class, new CustomDateDeserializer.LocalDateDeserializer());
-        // //Date序列化和反序列化
-        // javaTimeModule.addSerializer(Date.class,new CustomDateSerializer.DateSerializer());
-        // javaTimeModule.addDeserializer(Date.class,new CustomDateDeserializer.DateDeserializer());
+        // LocalDateTime 这里只需要LocalDateTime 如果需要转其他的,相应修改
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
 
         objectMapper.registerModule(javaTimeModule);
         return objectMapper;
     }
 
     /**
-     * 其他  比如`@RequestParam`和`@PathVariable`以及不加注解的单参数和对象参数
+     * 入参 时间戳 -> LocalDateTime
+     */
+    public static class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+        @Override
+        public LocalDateTime deserialize(JsonParser p, DeserializationContext context) throws IOException {
+            long timestamp = p.getValueAsLong();
+            if (timestamp >= 0) {
+                return LocalDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
+            } else {
+                return null;
+            }
+        }
+    }
+
+    /**
+     * @author kun.han on 2020/3/4 15:45
+     * 返回参数 LocalDateTime -> 时间戳
+     */
+    public static class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+        @Override
+        public void serialize(LocalDateTime localDateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+            if (Objects.nonNull(localDateTime)) {
+                jsonGenerator.writeNumber(localDateTime.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli());
+            }
+        }
+    }
+
+    /**
+     * 2>  比如`@RequestParam`和`@PathVariable`以及不加注解的单参数和对象参数
      */
     @Bean
     public Converter<String, LocalDateTime> localDateConverter() {
         //此处不能替换为lambda表达式
-        return new CustomDateConverter.LocalDateConvert();
+        return new LocalDateConvert();
+    }
+
+    /**
+     * 2>  比如`@RequestParam`和`@PathVariable`以及不加注解的单参数和对象参数
+     */
+    public static class LocalDateConvert implements Converter<String, LocalDateTime> {
+        @Override
+        public LocalDateTime convert(String timestamp) {
+            return LocalDateTime.ofInstant(Instant.ofEpochMilli(Long.parseLong(timestamp)), ZoneId.systemDefault());
+        }
+
+        @Override
+        public JavaType getInputType(TypeFactory typeFactory) {
+            return null;
+        }
+
+        @Override
+        public JavaType getOutputType(TypeFactory typeFactory) {
+            return null;
+        }
     }
 }
